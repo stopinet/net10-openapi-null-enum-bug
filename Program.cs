0274e616d8e176615c36e417b4a3b8729459ae13
Stefan Opitz <17011922+stopinet@users.noreply.github.com>
2026-01-12 09:03:40 +0000

    Reproduce null enum bug with simple solution

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Register the OpenAPI generator
+builder.Services.AddOpenApi("enums");
+builder.Services.ConfigureHttpJsonOptions(options =>
+{
+    options.SerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+    options.SerializerOptions.Converters.Add(new JsonStringEnumConverter());
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline
+if (app.Environment.IsDevelopment())
+{
+    // Enable the OpenAPI JSON endpoint
+    app.MapOpenApi();
+}
+
+// Swapping query1 and query2 order of registration, adds the null value back into the generated OpenAPI spec.
+
+// Adds null value to XYZ enum
+app.MapGet("/query1", (ABC abc, XYZ xyz, XYZ? nullableXyz) => $"HELLO {abc} | {xyz} | {nullableXyz}");
+
+// Does not add null value to XYZ enum, but also doesn't declare nullableXyz as oneOf with null value and reference.2
+app.MapGet("/query2", (ABC abc, XYZ? nullableXyz, XYZ xyz) => $"HELLO {abc} | {xyz} | {nullableXyz}");
+
+app.Run();
+
+public enum ABC
+{
+    A,
+    B,
+    C
+}
+
+public enum XYZ
+{
+    X,
+    Y,
+    Z
+}
--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# net10-openapi-null-enum-bug
+# .NET 10 OpenAPI null enum bug
+
 Provides example of null enum bug
+
+## Reproduction:
+
+when `query2` gets registered before `query1` the openapi json adds a `null` value to the `XYZ` enum
+
+```json
+"components": {
+"schemas": {
+    "ABC": {
+    "enum": [
+        "A",
+        "B",
+        "C"
+    ]
+    },
+    "XYZ": {
+    "enum": [
+        "X",
+        "Y",
+        "Z",
+        null
+    ]
+    }
+}
+
+```
+
+however when `query1` is registered before `query2` the null value is not there (expected).
+
+This can also be achieved by simple having the nullable parameters before the non nullable parameters.
